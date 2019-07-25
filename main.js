fe6cbcf0d231ba6d8772d3c111208a82f89409ae
Sjwens <1017301325@qq.com>
2019-07-25 22:07:19 +0800

    新增案例

--- main.js
+++ main.js
@@ -18,9 +18,78 @@ Vue.component('product-a', {
     },
 
 })
+Vue.component(
+    'async-example',
+    () => import('./my-async-component')
+)
+Vue.component('custom-input', {
+    props: ['value'],
+    template: `
+      <input
+        v-bind:value="value"
+        v-on:input="$emit('input', $event.target.value)"
+      >
+    `
+})
+
+Vue.component('base-checkbox', {
+    model: {
+      prop: 'checked',
+      event: 'change'
+    },
+    props: {
+      checked: Boolean
+    },
+    template: `
+      <input
+        type="checkbox"
+        v-bind:checked="checked"
+        v-on:change="$emit('change', $event.target.checked)"
+      >
+    `
+})
+Vue.component('base-input', {
+    inheritAttrs: false,
+    props: ['label', 'value'],
+    computed: {
+      inputListeners: function () {
+        var vm = this
+        // `Object.assign` 将所有的对象合并为一个新对象
+        return Object.assign({},
+          // 我们从父级添加所有的监听器
+          this.$listeners,
+          // 然后我们添加自定义监听器，
+          // 或覆写一些监听器的行为
+          {
+            // 这里确保组件配合 `v-model` 的工作
+            input: function (event) {
+              vm.$emit('input', event.target.value)
+            }
+          }
+        )
+      }
+    },
+    template: `
+      <label>
+        {{ label }}
+        <input
+          v-bind="$attrs"
+          v-bind:value="value"
+          v-on="inputListeners"
+        >
+      </label>
+    `
+})
 var app = new Vue({
     el:'#app',
     data:{
-        dataMax:'3434'
+        dataMax:'3434',
+        searchText: '',
+        lovingVue: false
     },
+    methods:{
+        onFocus: function (ev) {
+           alert('11111')
+        }
+    }
 })
